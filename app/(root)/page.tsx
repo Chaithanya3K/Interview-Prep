5343b1e5689d92eeee137162b45c05a724a54880
Chaithanya3K <chaithanya3k@gmail.com>
2025-11-29 00:33:41 +0530

    setup app,file,folder,structure,routing and autoPage

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,9 +1,23 @@
-import React from 'react'
+import React from 'react';
+import Link from 'next/link';
+import { Button } from '@/components/ui/button'; // make sure you import Button
 
-const page = () => {
+const Page = () => {
   return (
-    <div>Home Page</div>
-  )
-}
+    <>
+      <section className="card-cta">
+        <div className="flex flex-col gap-6 max-w-lg">
+          <h2>Get Interview-Ready with AI-Powered Practice and Feedback</h2>
+          <p className="text-lg">
+            Practice on real interview questions and get instant feedback
+          </p>
+          <Button asChild className="btn-primary max-sm:w-full">
+            <Link href="/interview">Start an Interview</Link>
+          </Button>
+        </div>
+      </section>
+    </>
+  );
+};
 
-export default page
+export default Page;
